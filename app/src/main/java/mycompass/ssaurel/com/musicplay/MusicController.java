2bcc05a3b5130aafc2dcd4d8489ab36d4611b40b
Lizaveta Panineyeva <liza.panineyeva@gmail.com>
2017-07-05 19:33:57 +0300

    Music player android app

---
+++ app/src/main/java/mycompass/ssaurel/com/musicplay/MusicController.java
@@ -0,0 +1,16 @@
+package mycompass.ssaurel.com.musicplay;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by lizaveta on 17.06.2017.
+ */
+
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
